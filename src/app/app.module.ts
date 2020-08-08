3d577abe091b1298bd482844eb1a968a2c88dc15
jufergom <jufergom@hotmail.com>
2020-08-07 19:48:05 -0600

    created needed components and classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListContactsComponent } from './list-contacts/list-contacts.component';
+import { FilterComponent } from './filter/filter.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListContactsComponent,
+    FilterComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule
